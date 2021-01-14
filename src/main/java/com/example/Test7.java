1dc6ef2926c6f2ed5103ee31c86f516c0b2854f4
wy <2586645609@qq.com>
2021-01-14 15:11:09 +0800

    yyy

--- src/main/java/com/example/Test7.java
+++ src/main/java/com/example/Test7.java
@@ -2,12 +2,7 @@ package com.example;
 
 
 import java.io.*;
-import java.time.Clock;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Predicate;
 
 public class Test7{
 
@@ -17,8 +12,8 @@ public class Test7{
         String file = "E:\\data\\result\\motion_svm.txt";
         int count = 0;
 
-        List<Double[]> data = getData(file);
-
+        //List<Double[]> data = getData(file);
+/*
         for(int i = 0;i < data.size();i++){
             Double[] d = data.get(i);
             double label = d[1] > 0.5 ? 1.0 : 0.0;
@@ -27,14 +22,14 @@ public class Test7{
             }
         }
        // String a = "aaabb";
-        System.out.println(count);
+        System.out.println(count);*/
 
 
 
     }
 
-    public static List<Double[]> getData(String fileName) throws IOException {
-        List<Double[]> list = new ArrayList<>();
+    public static void getData(String fileName) throws IOException {
+     //   List<Double[]> list = new ArrayList<>();
         File file = new File(fileName);
         FileInputStream fis = new FileInputStream(file);
         BufferedReader br = new BufferedReader(new InputStreamReader(fis));
@@ -46,9 +41,9 @@ public class Test7{
             for(int i = 0;i < index;i++){
                 d[i] = Double.parseDouble(str[i]);
             }
-            list.add(d);
+        //    list.add(d);
         }
-        return list;
+        //return list;
     }
 
 }
